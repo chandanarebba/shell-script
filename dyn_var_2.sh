c1078b46feb4f7eebf1e07791723fab1086abfb5
chandanarebba <chandanarebba111@gmail.com>
2025-11-21 21:05:29 +0000

    s

--- dyn_var_2.sh
+++ dyn_var_2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-start_time= $(date +%s)
+start_time=$(date +%s)   # No space around '='
 sleep 10
-end_time=$(date +%s)
-total_time= (($end_time - $start_time))
-echo "script executed in $total_time in seconds"
+end_time=$(date +%s)     # No space around '='
+
+# Use $(( ... )) for arithmetic
+total_time=$((end_time - start_time))
+
+echo "Script executed in $total_time seconds"
